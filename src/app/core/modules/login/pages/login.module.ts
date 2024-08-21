ffb5c966b465933cb7f42384204879e5b1a6ef75
Marcos Rincón <softwarenexosdev3@gmail.com>
2024-08-21 16:25:47 -0500

    Design base

---
+++ src/app/core/modules/login/pages/login.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { FormLoginComponent, RecoveryLoginComponent } from '@modules';
+
+@NgModule({
+  declarations: [
+    FormLoginComponent,
+    RecoveryLoginComponent
+  ],
+  imports: [],
+})
+export class LoginModule {}
